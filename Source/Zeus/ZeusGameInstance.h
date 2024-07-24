db6e6e35d6793d19e3b8e49222d124f41bbab37c
Dmytro <dvshalimov@gmail.com>
2024-07-24 21:48:59 +0200

    game instance + levels

---
+++ Source/Zeus/ZeusGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ZeusGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZEUS_API UZeusGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
